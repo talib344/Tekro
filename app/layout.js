d1b00df8a79a38e96009e70ea1be6ef4a9005f2a
talib344 <talibsindhi344@gmail.com>
2026-07-04 19:17:09 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,20 @@
 import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Tekro-AI 2030',
-  description: 'By Talib Ali',
+  description: '2x Faster | 10x Better | 100+ Languages | By Talib Ali 🇵🇰',
+  keywords: 'AI, ChatGPT Alternative, Groq, Llama 3, Pakistan AI',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   )
 }
