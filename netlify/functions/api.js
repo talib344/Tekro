d9bb8440215fb55769d5ca3d919b0e2dd45248ae
talib344 <talibsindhi344@gmail.com>
2026-07-04 06:21:48 +0500

    Create api.js

---
+++ netlify/functions/api.js
@@ -0,0 +1,26 @@
+exports.handler = async (event, context) => {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
+      },
+      body: ""
+    };
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      status: "ok",
+      message: "Tekro AI Backend on Netlify 🔥",
+      time: new Date().toISOString()
+    })
+  };
+};
